498b98d6d7e73b9cd2d8f66cf7739e47f485b802
LitStronger <1589015065@qq.com>
2020-04-19 14:16:49 +0800

    nav

---
+++ vue-nav/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <myNav />
+    <!-- <list /> -->
+    <!-- <vModelTest msg="init" /> -->
+  </div>
+</template>
+<script>
+import myNav from './components/myNav'
+// import list from './components/v-for.vue'
+// import vModelTest from './components/v-model'
+export default {
+  name: 'App',
+  components: {
+    // list,
+    // vModelTest,
+    myNav
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue-nav/src/components/myNav.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav v-bind:class="active">
+        <a class="home" v-on:click="select('home')" href="#">home</a>
+        <a class="projects" v-on:click="select('projects')" href="#">projects</a>
+        <a class="services" v-on:click="select('services')" href="#">services</a>
+        <a class="contact" v-on:click="select('contact')" href="#">contact</a>
+    </nav>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            active: 'home'
+        }
+    },
+    methods: {
+        select: function(item){
+            this.active = item
+            console.log('click: '+item);
+        }
+    }
+}
+</script>
+<style>
+	nav{
+		font:15px/1.3 'Open Sans', sans-serif;
+		text-align:center;
+        width: 100%;
+        background-color: #068bbb;
+	}
+
+	nav a{
+		display:inline-block;
+		padding: 18px 30px;
+		color:#fff !important;
+		font-weight:bold;
+		font-size:16px;
+		text-decoration:none !important;
+		line-height:1;
+		text-transform: uppercase;
+		background-color:transparent;
+	}
+
+    a:hover{
+        background-color: #096c9e;
+	}
+	.home .home,            /*第一个值是nav的，第二个值是a标签的*/
+	.projects .projects,
+	.services .services,
+	.contact .contact{
+		background-color:#e35885;
+	}
+</style>
